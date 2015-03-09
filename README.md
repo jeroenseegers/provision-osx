2966314599da7264feb98bf161bb8f0131ee41ad
Jeroen Seegers <jeroen.seegers+github@gmail.com>
2015-03-09 22:39:21 +0100

    Update README, add provision script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # provision-osx
 Provisioning details for OSX
+
+```shell
+git clone git@github.com:jeroenseegers/provision-osx.git
+cd provision-osx
+./provision.sh
+```
---
+++ provision.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Set bash 'strict-mode' (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
+set -euo pipefail
+
+if [[ ! -x /usr/bin/gcc ]]; then
+    # Install xcode command-line tools
+    xcode-select --install
+fi
+
+if [[ ! -x /usr/local/bin/brew ]]; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Set the correct path for using homebrew packages
+export PATH=/usr/local/bin:$PATH
+
+if [[ ! -x /usr/local/bin/ansible ]]; then
+    # Install Ansible
+    brew install ansible
+fi
+
+# Run Ansible
+ansible-playbook -i ansible_hosts site.yml --extra-vars "user=${USER}"
